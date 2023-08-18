ac7d6d50ce8d107c0a9dfad21c0bf16770bff2d1
Jack Pickard <jack@hertilityhealth.com>
2023-08-18 10:20:34 +0100

    remove icon

--- src/pages/components/Nav/Nav.tsx
+++ src/pages/components/Nav/Nav.tsx
@@ -1,22 +1,10 @@
-import Image from "next/image";
-import githubIcon from "../../../images/github-mark-white.svg";
 import styles from "./nav.module.css";
 
 export const NavBar = () => {
   return (
     <div className={styles.navbar}>
       <div className={styles.NavTitle}>Note Nomad</div>
-      <div>
-        <a href="https://github.com/jmjpickard/note-your-day" target="_blank">
-          <Image
-            priority
-            src={githubIcon}
-            height={32}
-            width={32}
-            alt="Source code"
-          />
-        </a>
-      </div>
+      <div></div>
     </div>
   );
 };
