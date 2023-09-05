f809fb0908449e4c054d3c779a23fff97dda6b01
Jack Pickard <jack@hertilityhealth.com>
2023-09-05 08:39:00 +0100

    more

--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -9,16 +9,13 @@ const Index = () => {
       const today = new Date().toISOString().split("T")[0] || "";
       // Create an async function and immediately call it with try...catch
       const handleRouterPush = async () => {
-        try {
-          await router.push(`/notes/${today}`);
-          // This code will be executed after the router push is complete
-        } catch (error) {
-          // Handle any potential errors here
-          console.error("Router push error:", error);
-        }
+        await router.push(`/notes/${today}`);
+        // This code will be executed after the router push is complete
       };
 
-      handleRouterPush();
+      handleRouterPush()
+        .then()
+        .catch((err) => console.log(err));
     }
   }, [router]);
 
